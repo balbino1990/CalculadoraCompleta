1df80cb16ddf611cc71685bdf2563ca194dce464
balbino1990 <balbino.pinto@yahoo.com>
2017-04-18 10:35:30 +0100

    Ultima versão

--- CalculadoraCompleta-28032017/CalculadoraCompleta-28032017/Controllers/HomeController.cs
+++ CalculadoraCompleta-28032017/CalculadoraCompleta-28032017/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace CalculadoraCompleta_28032017.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+        // GET: Home/
         public ActionResult Index()
         {
             //inicializar valores
